93f61dcceb999845a2998b73e2c7dd28ecbfa9e4
Александр Мыльников <ingaksdr@yandex.ru>
2023-04-28 15:14:03 +0300

    Creating IsAdminModeratorOrAuthor

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,6 +1,19 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
-class IsAdminUser(BasePermission):
+class IsAdminUser(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == 'admin'
+
+
+class IsAdminModeratorOrAuthor(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return ((obj.author == request.user)
+                or (request.user.role == 'admin')
+                or (request.user.role == 'moderator'))
