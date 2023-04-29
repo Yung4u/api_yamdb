ec004769803955e33191cc5b0e031052dbadc24b
Орлов Сергей <jokeblablo@gmail.com>
2023-04-29 22:02:04 +0300

    fix

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # api_yamdb
 api_yamdb
+asdasd
+
+asdasda
+
+asdas
+das
+das
+das
+da
+sd
+asdf
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User, Review, Title, Comment
+from .models import User, Review, Title, Comment, Genre, Category
 
 
 admin.site.register(User)
 admin.site.register(Review)
 admin.site.register(Comment)
 admin.site.register(Title)
+admin.site.register(Genre)
+admin.site.register(Category)
