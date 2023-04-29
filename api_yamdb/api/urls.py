60f2c2b3875378a92f3ebace53ff971e8f1a0590
Орлов Сергей <jokeblablo@gmail.com>
2023-04-29 20:35:32 +0300

    delete any files

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from api.views import (CommentViewSet, TitleViewSet,
-                       GenreViewset, CategoryViewSet,
+                       GenreViewSet, CategoryViewSet,
                        ReviewViewSet, UserViewSet,
                        SignupViewSet, TokenViewSet)
 
